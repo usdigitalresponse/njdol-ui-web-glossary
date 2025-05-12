43468527625631fa4041961a98cddebe7eb13d1b
Chris Broome <chris.a.broome@gmail.com>
2025-05-12 10:00:01 -0400

    Simple toggle functionality

--- js/language-toggler.js
+++ js/language-toggler.js
@@ -1,6 +1,20 @@
 $(document).ready(() => {
-  $(".lang-toggle").click(() => {
-    console.log("clicked");
+  $(".lang-toggle").click((el) => {
+    //console.log("clicked", $el.currentTarget.attributes["data-lang"].nodeValue);
+
+    const lang = el.currentTarget.attributes["data-lang"].nodeValue;
+
+    const $matchedParent = $(el.currentTarget).parents("li.term");
+
+    const $matchedLang = $matchedParent.find(`.definition.${lang}`);
+
+    console.log({ $matchedLang, $matchedParent });
+
+    $matchedParent.find(".definition").hide();
+    $matchedLang.show();
+
+    //console.log({ $matchedLang });
+
     return false;
   });
 });
