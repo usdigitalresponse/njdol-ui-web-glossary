0abd1eb1cfce5e99fd2a4acb0d69d3e94368e569
Chris Broome <chris.a.broome@gmail.com>
2025-05-19 11:48:55 -0400

    Global toggle; create the language options based on the structure of the term

--- js/language-toggler.js
+++ js/language-toggler.js
@@ -1,19 +1,46 @@
 $(document).ready(() => {
-  $(".lang-toggle").click((el) => {
-    //console.log("clicked", $el.currentTarget.attributes["data-lang"].nodeValue);
+  // Setup links
+  $(".terms li.term").each((_idx, term) => {
+    const definitions = $(term)
+      .find(".definition")
+      .map((idx, definition) => {
+        const classNames = definition.className.split(" ");
+        // Fragile code, assumes the language will be the last class name
+        return classNames[classNames.length - 1];
+      })
+      .get();
 
+    if (definitions) {
+      const ul = document.createElement("ul");
+      ul.className = "language-select";
+
+      definitions.forEach((definition) => {
+        ul.innerHTML += `
+          <li>
+            <a href="" class="lang-toggle" data-lang="${definition}">${definition}</a>
+          </li>
+        `;
+      });
+
+      term.append(ul);
+    }
+  });
+
+  $(".apply-lang").click((el) => {
+    const selectedLanguage = $(".lang-toggle-global").val();
+    $(".definition").hide();
+    $(`.definition.${selectedLanguage}`).show();
+  });
+
+  $(".lang-toggle").click((el) => {
     const lang = el.currentTarget.attributes["data-lang"].nodeValue;
 
     const $matchedParent = $(el.currentTarget).parents("li.term");
 
     const $matchedLang = $matchedParent.find(`.definition.${lang}`);
 
-    console.log({ $matchedLang, $matchedParent });
-
     $matchedParent.find(".definition").hide();
     $matchedLang.show();
-
-    //console.log({ $matchedLang });
 
     return false;
   });
