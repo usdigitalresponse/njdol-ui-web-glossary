2d3c722393e32be82ee3392aa0d923ca48d8f1b2
Chris Broome <chris.a.broome@gmail.com>
2025-06-15 18:49:46 -0400

    Basic search functionality; Toggle definition also toggles name

--- js/language-toggler.js
+++ js/language-toggler.js
@@ -1,8 +1,20 @@
 // @ts-nocheck
 $(document).ready(() => {
   const $terms = $("ul.terms");
+  const $glossarySearchMatchCount = $(".glossary-search-match-count");
+  const $glossarySearchSummary = $(".glossary-search-summary");
+  const $glossarySearchReset = $(".glossary-search-reset");
+  const $glossarySearchInput = $(".glossary-search-input");
 
-  // Setup links
+  let selectedLanguage = $terms.attr("data-lang");
+
+  if (!selectedLanguage) {
+    console.error("No selected language set on ul.terms");
+  }
+
+  /**
+   * Setup links
+   */
   $(".terms li.term").each((_idx, term) => {
     const definitions = $(term)
       .find(".definition")
@@ -33,7 +45,7 @@ $(document).ready(() => {
    * Toggle all definitions at once.
    */
   $(".apply-lang").click((el) => {
-    const selectedLanguage = $(".lang-toggle-global").val();
+    selectedLanguage = $(".lang-toggle-global").val();
 
     $(".definition").hide();
     $(`.definition.${selectedLanguage}`).show();
@@ -49,17 +61,61 @@ $(document).ready(() => {
   });
 
   /**
-   * Toggle the language within an individual block
+   * Toggle the language within an individual definition
    */
   $(".lang-toggle").click((el) => {
     const lang = el.currentTarget.attributes["data-lang"].nodeValue;
-
     const $matchedParent = $(el.currentTarget).parents("li.term");
     const $matchedLang = $matchedParent.find(`.definition.${lang}`);
 
     $matchedParent.find(".definition").hide();
+
+    $matchedParent.find("ul.term-name li").hide();
+    $matchedParent.find(`ul.term-name li.${lang}`).show();
+    // always show the English option
+    $matchedParent.find(`li.english`).show();
     $matchedLang.show();
 
     return false;
+  });
+
+  /**
+   * Search functionality
+   */
+  $(".glossary-search-button").click(() => {
+    const searchTerm = $glossarySearchInput.val();
+    let matches = 0;
+
+    if (searchTerm) {
+      const lowerCaseSearchTerm = searchTerm.toLocaleLowerCase();
+      $terms.find("li.term").each((_idx, term) => {
+        const $term = $(term);
+        $term
+          .find(`.term-name .${selectedLanguage}`)
+          .each((_idx, termNameEl) => {
+            const termName = termNameEl.innerHTML?.toLocaleLowerCase();
+
+            if (termName.includes(lowerCaseSearchTerm)) {
+              matches++;
+            } else {
+              $term.hide();
+            }
+          });
+      });
+
+      $glossarySearchSummary.show();
+      $glossarySearchMatchCount.html(matches);
+    } else {
+      matches = 0;
+    }
+  });
+
+  /**
+   * Reset search functionality
+   */
+  $glossarySearchReset.click(() => {
+    $glossarySearchSummary.hide();
+    $terms.find("li.term").show();
+    $glossarySearchInput.val("");
   });
 });
